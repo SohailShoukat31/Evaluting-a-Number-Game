77b9caab57f5997c8507f20b14c5f9958bbab9e1
sohailahmed31 <160041648+sohailahmed31@users.noreply.github.com>
2024-05-11 10:37:36 +0500

    Initial commit

---
+++ main.ts
@@ -0,0 +1,9 @@
+let guess: number = 7;
+let target: number = 5;
+if (guess < target) {
+  console.log("Your guess is too low.");
+} else if (guess > target) {
+  console.log("Your guess is too high.");
+} else {
+  console.log("Your guessed correctly");
+}
